548df217d42e4cd856e238ef7612eb213123e49e
Alisson Youssf <youssfbr@gmail.com>
2021-03-08 11:32:22 -0300

    Project created, Web Dependency

---
+++ backend/src/main/java/com/github/youssf/works/WorksApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssf.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksApplication.class, args);
+	}
+
+}
